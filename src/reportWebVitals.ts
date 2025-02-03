bb50a3e4cbb5fb0cfc359a4f0044e10e8b4fad21
KaterynaSch <128621831+KaterynaSch@users.noreply.github.com>
2025-02-03 13:51:08 +0200

    initial commit

---
+++ src/reportWebVitals.ts
@@ -0,0 +1,16 @@
+import { onCLS, onFID, onFCP, onLCP, onTTFB } from "web-vitals";
+import { Metric } from "web-vitals";
+
+type ReportHandler = (metric: Metric) => void;
+
+const reportWebVitals = (onPerfEntry?: ReportHandler) => {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    onCLS(onPerfEntry);
+    onFID(onPerfEntry);
+    onFCP(onPerfEntry);
+    onLCP(onPerfEntry);
+    onTTFB(onPerfEntry);
+  }
+};
+
+export default reportWebVitals;
